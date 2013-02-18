47e172693271af883150c5d0bf52e243fc5d71f6
Marco Schuster <marco+github@m-s-d.eu>
2013-02-18 13:24:46 +0100

    example1: rewrote to use new module infrastructure

--- examples/example_1.php
+++ examples/example_1.php
@@ -1,7 +1,11 @@
 <?php
+//Load FUSE, if it isn't already loaded by php.ini
+if (!extension_loaded("fuse"))
+    dl("fuse.so");
+error_reporting(E_ALL);
+
 class Sample1Fuse extends Fuse {
 }
 
 $fuse = new Sample1Fuse();
-$fuse->mount("/tmp/phpfusemount", "allow_other");
-?>
+$fuse->fuse_main($argc,$argv);
