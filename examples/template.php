5677b3f14641bbd17c8cf48677be1d4d7e8e76c3
Marco Schuster <marco+github@m-s-d.eu>
2013-02-18 13:59:17 +0100

    Update examples/template.php

--- examples/template.php
+++ examples/template.php
@@ -8,14 +8,13 @@ class PHPFS extends Fuse {
     //basic needed stuff
     public $name = "phpfs";
     public $version = "1.3.37";
-
-  public function __construct() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
+    public function __construct() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
         $this->opt_keys = array_flip(array(
             "KEY_HELP",
             "KEY_FUSE_HELP",
             "KEY_VERSION",
-            "KEY_DEBUG",
+            "KEY_DEBUG"
         ));
         $this->opts     = array(
             "--help" => $this->opt_keys["KEY_HELP"],
@@ -23,15 +22,15 @@ class PHPFS extends Fuse {
             "-h" => $this->opt_keys["KEY_HELP"],
             "-H" => $this->opt_keys["KEY_FUSE_HELP"],
             "-V" => $this->opt_keys["KEY_VERSION"],
-            "-d" => $this->opt_keys["KEY_DEBUG"],
+            "-d" => $this->opt_keys["KEY_DEBUG"]
         );
         $this->userdata = array();
-  }
-  public function __destruct() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-  }
-      public function main($argc, $argv) {
-      printf("phpfuse: %s called\n",__FUNCTION__);
+    }
+    public function __destruct() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+    }
+    public function main($argc, $argv) {
+        printf("PHPFS: %s called\n", __FUNCTION__);
         $res = $this->opt_parse($argc, $argv, $this->userdata, $this->opts, array(
             $this,
             "opt_proc"
@@ -72,149 +71,120 @@ Options specific to %1\$s:
 [none]
 
 ", $this->name, $argv[0]);
-                
                 return 0;
                 break;
             case $this->opt_keys["KEY_VERSION"]:
                 printf("%s %s\n", $this->name, $this->version);
-                
                 return 1;
                 break;
             case $this->opt_keys["KEY_DEBUG"]:
                 printf("debug mode enabled\n");
-                
                 return 1;
                 break;
             default:
                 return 1;
         }
-    } 
-  public function getattr() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function readlink() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function getdir() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function mknod() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function mkdir() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function unlink() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function rmdir() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function symlink() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function rename() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function link() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function chmod() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function chown() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function truncate() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function utime() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function open() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function read() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function write() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function statfs() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function flush() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function release() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function fsync() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function setxattr() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function getxattr() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function listxattr() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
-  public function removexattr() {
-    printf("phpfuse: %s called\n",__FUNCTION__);
-    return -FUSE_ENOSYS;
-
-  }
+    }
+    public function getattr() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function readlink() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function getdir() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function mknod() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function mkdir() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function unlink() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function rmdir() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function symlink() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function rename() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function link() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function chmod() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function chown() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function truncate() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function utime() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function open() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function read() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function write() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function statfs() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function flush() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function release() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function fsync() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function setxattr() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function getxattr() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function listxattr() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
+    public function removexattr() {
+        printf("PHPFS: %s called\n", __FUNCTION__);
+        return -FUSE_ENOSYS;
+    }
 }
-
 $fuse = new PHPFS();
-$fuse->main($argc,$argv);
+$fuse->main($argc, $argv);
